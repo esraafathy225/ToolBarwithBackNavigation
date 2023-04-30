1f0504ab302f3f91cd9b5b4e41922f7ca221f884
esraa.fathy225 <esraa.fathy225@gmail.com>
2023-04-30 20:22:21 +0300

    First Version

--- app/src/main/java/com/esraa/hp/toolbarwithbacknavigation/MainActivity.java
+++ app/src/main/java/com/esraa/hp/toolbarwithbacknavigation/MainActivity.java
@@ -1,10 +1,10 @@
 package com.esraa.hp.toolbarwithbacknavigation;
 
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentTransaction;
-import android.support.v7.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.Toolbar;
+import androidx.appcompat.widget.Toolbar;
 import android.view.View;
 import android.widget.ImageView;
 
